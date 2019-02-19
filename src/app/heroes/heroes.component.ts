d16f65939594e9c6a19893b865799111eeba89df
Aphisit Namracha <aphisiit086757@hotmail.com>
2019-02-19 20:58:57 +0700

    Add trigger for refresh when data was change.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
@@ -9,11 +9,13 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  displayedColumns: string[] = ['index', 'name', 'action'];
   heroes: Hero[];
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService, private changeDetectorRefs: ChangeDetectorRef) { }
 
   ngOnInit() {
+    // this.refresh();
     this.getHeroes();
   }
 
@@ -28,11 +30,21 @@ export class HeroesComponent implements OnInit {
     this.heroService.addHero({ name } as Hero)
       .subscribe(hero => {
         this.heroes.push(hero);
+        this.refresh();
+        // TODO : You can use getHeroes method instead refresh
+        // this.getHeroes();
       });
   }
 
   delete(hero: Hero): void {
     this.heroes = this.heroes.filter(h => h !== hero);
     this.heroService.deleteHero(hero).subscribe();
+  }
+
+  refresh() {
+    // this.heroService.getHeroes().subscribe((res) => {
+      this.getHeroes();
+      this.changeDetectorRefs.detectChanges();
+    // });
   }
 }
